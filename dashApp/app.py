ef615bea56c923ac81175fd03b87c1cae21cd84c
Xuyen <93767417+Xuyen21@users.noreply.github.com>
2025-11-18 12:17:49 +0100

    enable debug in pycharm

--- dashApp/app.py
+++ dashApp/app.py
@@ -32,4 +32,6 @@ def render_nav(pathname):
     ]
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,
+            use_reloader=False) #important for debugging in pycharm
+
