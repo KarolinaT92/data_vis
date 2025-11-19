9717f294e0f3ff8a7aadb0fd62cee4576688a6b0
Xuyen <93767417+Xuyen21@users.noreply.github.com>
2025-11-19 21:46:54 +0100

    Add shared package

---
+++ shared/read_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+def load_data():
+    data_path = "..\\superstore_dataset\\cleaned_Superstore.csv"
+    df = pd.read_csv(data_path, parse_dates=['Order Date', 'Ship Date'])
+
+    df["ProfitMargin"] = np.where(df["Sales"] > 0, (df["Profit"] / df["Sales"]) * 100, np.nan)
+    df[["Sales", "Profit"]] = df[["Sales", "Profit"]].round(2)
+    df['Month'] = df['Order Date'].dt.month
+    df['Year'] = df['Order Date'].dt.year
+    df["Product_Key"] = df["Product ID"] + " | " + df["Product Name"]
+    # Original Unit Price
+    df["Original Unit Price"] = df["Sales"] / ((1 - df["Discount"]) * df["Quantity"])
+    df['Month_Name'] = pd.to_datetime(df['Month'], format='%m').dt.strftime('%b')
+
+    df["Profit Margin (%)"] = (df["Profit"] / df["Sales"]) * 100
+    df["Profit Margin (%)"] = df["Profit Margin (%)"].round(2)
+    return df
+
+
+df = load_data()
